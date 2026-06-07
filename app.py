801ba201d676e179bdd92bb53a00d391d6d2cc66
mrdoework-rgb <mrdoework@gmail.com>
2026-06-07 19:15:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,8 @@ st.set_page_config(page_title="Revision Template Generator", layout="centered")
 st.title("📄 Generate Recall Questions from JSON")
 
 st.markdown("Upload a template or use the template from the repository")
-st.markdown("It will output a JSON which you should paster in the box below")
+st.markdown(Create the Questions [Using this Gemini Gem](https://gemini.google.com/gem/1CP6tJCFXiPZY-aRzbab-6fUZJyB0ILL4?usp=sharing).)
+st.markdown("It will output a JSON which you should paste in the box below")
 col1, col2 = st.columns(2)
 with col1:
     use_repository_template = st.checkbox("Use template from repository", value=False)
